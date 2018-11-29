0e9793cd34e00c908a6a78bf7cb0564ae999bd41
tod.shen <tod.shen@104.com.tw>
2018-11-29 14:22:50 +0800

    Refactor: adject regular expression

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s/endpoint: /endpoint: ${PRISMA_ENDPOINT}/" /app/prisma.yml
+sed -i "s/endpoint: \S*/endpoint: ${PRISMA_ENDPOINT}/" /app/prisma.yml
 prisma deploy
 prisma generate
 npm start
